95eb589468ccaef65a3d7b166c2e93ab2850273c
gmbriyaz <168628474+gmbriyaz@users.noreply.github.com>
2024-05-01 14:10:37 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello riyaz devops ");
-      System.out.println("Hello riyaz ur code is ruuning know u write new code  ");
+      System.out.println("Hello riyaz ur code is ruuning know u write new code from help taken by moonlight is a devops eng  ");
     }  
 } 
